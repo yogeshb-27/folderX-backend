cfe5bff705afc863af4b864c67d369a3072b0f91
yogesh <bidaveyogesh@gmail.com>
2024-03-21 15:22:29 +0530

    added bcrypt to hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // Register a new user
 const register = async (req, res) => {
@@ -8,7 +9,10 @@ const register = async (req, res) => {
     if (user) {
       return res.status(401).json({ error: "Email Allready Registered" });
     }
-    await User.create({ email, password });
+    const newUser = await User.create({
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
